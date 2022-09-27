ba22668ea43761adcd4572cd5b5e991b77d6550e
fan-tech <fan.n.o.to.39@gmail.com>
2022-09-27 15:08:44 +0900

    staticにした

--- jankenneo/settings.py
+++ jankenneo/settings.py
@@ -113,7 +113,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'  # localでcssを読み込むためのパス(たぶん)
 
 # Extra places for collectstatic to find static files.
