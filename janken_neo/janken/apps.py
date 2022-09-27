1a7469999ccff72c4b577f5ecad2dda16c438080
fan-tech <fan.n.o.to.39@gmail.com>
2022-09-27 11:14:45 +0900

    フォルダの名前を変更した

--- janken_neo/janken/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JankenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'janken'
--- janken_neo/janken/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-urlpatterns = [
-    path('janken', views.index),
-    path('ajax-janken/', views.ajax_janken, name='ajax_janken'),
-
-]
