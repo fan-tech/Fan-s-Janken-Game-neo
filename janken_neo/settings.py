da3ae5aefb0b1979a90aeeacdd6dd0819fdcbf9f
fan-tech <fan.n.o.to.39@gmail.com>
2022-09-27 12:13:40 +0900

    templateを記載

--- janken_neo/settings.py
+++ janken_neo/settings.py
@@ -145,3 +145,19 @@ except ImportError:
 if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
