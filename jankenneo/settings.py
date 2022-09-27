ec8ac841fb2e41b0c2482176f130ed29a62bf154
fan-tech <fan.n.o.to.39@gmail.com>
2022-09-27 16:54:33 +0900

    修正

--- jankenneo/settings.py
+++ jankenneo/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'scheduleCalendar.apps.SchedulecalendarConfig',
+    'jankenneo.apps.jankenneoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
